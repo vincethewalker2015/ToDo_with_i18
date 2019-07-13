e076ff0b8ca4797b2c62df5bab0c1be2888cb262
Vincent <vincethewalker@yahoo.co.uk>
2019-07-13 13:24:41 +0100

    Set up I18n configurations in Application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /en|de/ do
+  scope "(:locale)", locale: /#{I18n.available_locales.join{"|"}}/ do
     root 'pages#home'
     get '/about', to: 'pages#about'
     get '/help', to: 'pages#help'
